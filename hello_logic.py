8b8810d39ec5a4b14deccb57179f43acefc230e9
voyagerdva <mariner-9@yndex.ru>
2021-11-05 17:38:42 +0300

    refactored to oop

--- hello_logic.py
+++ hello_logic.py
@@ -1,9 +1,7 @@
 import NeuroNetLibrary as nn
-import NeuroNluLibrary as nlu
 import NeuroVoiceLibrary as nv
 
 from hangup_logic import Hangup_logic as HU
-from forward_logic import Forward_logic as FWD
 from recommend_logic import Recommend_logic as RCMD
 
 
@@ -17,7 +15,11 @@ class Hello_logic():
             nv.say('hello_main')
         return r
 
-    def hello_null(self, ent_list, r):
+    def hello_null(self, r, ent_list):
+        if not r:
+            nn.log('condition', 'NULL')
+            return HU.hangup_null(r, ent_list)
+
         with nv.listen((None, None, 500, 'AND'), entities=ent_list) as r:
             nv.say('hello_null')
 
@@ -43,3 +45,28 @@ class Hello_logic():
             nn.log('condition', 'confirm=true')
             nv.say('hello_repeat')
         return RCMD.recommend_main(r)
+
+
+        with nv.listen((None, None, 500, 'AND'), entities=ent_list) as r:
+            nv.say('hello_null')
+
+        if r.has_entity('voicemail') and r.entity('confirm') == 'true':
+            nn.log('condition', 'confirm=true')
+            return RCMD.recommend_main(r)
+
+        if r.has_entity('voicemail') and r.entity('DEFAULT') == 'true':
+            nn.log('condition', 'DEFAULT=true')
+            return RCMD.recommend_main(r)
+
+        if r.has_entity('voicemail') and r.entity('confirm') == 'false':
+            nn.log('condition', 'confirm=false')
+            return HU.hangup_wrong_time(r)
+
+        if r.has_entity('voicemail') and r.entity('wrong_time') == 'true':
+            nn.log('condition', 'wrong_time=true')
+            return HU.hangup_wrong_time(r)
+
+        if r.has_entity('voicemail') and r.entity('repeat') == 'true':
+            nn.log('condition', 'repeat=true')
+            return self.hello_repeat(r)
+
--- recommend_logic.py
+++ recommend_logic.py
@@ -11,20 +11,25 @@ class Recommend_logic():
     def __init__(self):
         pass
 
-    def recommend_main(self, r):
+
+    def recommend_main(self, ent_list):
+        with nv.listen((None, None, 500, 'AND'), entities=ent_list) as r:
+            nv.say('recommend_main')
+        return r
 
 
     def recommend_default(self, ent_list):
         with nv.listen((None, None, 500, 'AND'), entities=ent_list) as r:
             nv.say('recommend_default')
 
+
         if r.has_entity('voicemail') and r.entity('repeat') == 'true':
             nn.log('condition', 'repeat=true')
-            return self.recommend_repeat(self, r)
+            return self.recommend_repeat(r)
 
         if r.has_entity('voicemail') and r.entity('recommendation') == 'dont_know':
             nn.log('condition', 'recommendation=dont_know')
-            return self.recommend_repeat_2(self, r)
+            return self.recommend_repeat_2(r)
 
         if r.has_entity('voicemail') and r.entity('wrong_time') == 'true':
             nn.log('condition', 'wrong_time=true')
@@ -34,30 +39,51 @@ class Recommend_logic():
             nn.log('condition', 'question=true')
             return FWD.forward_logic(r)
 
+    def recommend_null(self, r):
+        if r.has_entity('voicemail') and r.entity('repeat') == 'true':
+            nv.say('recommend_null')
+            nn.log('condition', 'repeat=true')
+            return self.recommend_repeat(r)
 
     def recommend_repeat(self, r):
-        pass
+        if not r:
+            nn.log('condition', 'NULL')
+            return self.recommend_default(r)
+
+        with nv.listen((None, None, 500, 'AND'), entities=ent_list) as ans:
+            nv.say('recommend_default')
+
+        if ans.has_entity('voicemail') and ans.entity('DEFAULT') == 'true':
+            nn.log('condition', 'repeat=true')
+            nv.say('recommend_repeat')
+            return HU.hangup_null(r)
+
 
 
     def recommend_repeat_2(self, r):
-        pass
-
-    def recommend_main(self):
-        pass
-
-
-    def recommend_score_negative(self):
-        pass
+        if r.has_entity('voicemail') and (r.entity('SCORE') >=0 and r.entity('SCORE') <=8):
+            nn.log('condition', 'repeat=true')
+            nv.say('recommend_repeat_2')
+            return HU.hangup_negative(r)
 
 
-    def recommend_score_neutral(self):
-        pass
+    def recommend_score_negative(self, r):
+        if r.has_entity('voicemail') and (r.entity('SCORE') >=8 and r.entity('SCORE') <=10):
+            nn.log('condition', 'repeat=true')
+            nv.say('recommend_score_negative')
+            return HU.hangup_positive(r)
 
 
-    def recommend_null(self):
-        pass
+    def recommend_score_neutral(self, r):
+        if r.has_entity('voicemail') and (r.entity('SCORE') == "false"):
+            nn.log('condition', 'repeat=true')
+            nv.say('recommend_score_neutral')
+            return self.recommend_score_negative(r)
 
 
     def recommend_score_positive(self):
-        pass
+        if r.has_entity('voicemail') and (r.entity('recomendation') == "neutral"):
+            nn.log('condition', 'repeat=true')
+            nv.say('recommend_score_positive')
+            return self.recommend_score_neutral(r)
 
